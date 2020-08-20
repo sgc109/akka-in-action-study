d84365e91374d950c56204f0852e196ab97766be
Sungho Hwang <sgc109109@gmail.com>
2020-08-20 11:18:41 +0900

    edit .gitignore

--- sungho/chapter-02/src/main/scala/com/sungho/BoxOffice.scala
+++ sungho/chapter-02/src/main/scala/com/sungho/BoxOffice.scala
@@ -1,11 +1,13 @@
 package com.sungho
 
 import akka.actor.{Actor, Props}
+import com.sungho.BoxOffice.GetEvents
 
 object BoxOffice {
   def props = Props(new BoxOffice)
   def name = "boxOffice"
 
+  case object GetEvent
   case object GetEvents
 
   case class Event(name:String, tickets: Int)
@@ -14,8 +16,17 @@ object BoxOffice {
 }
 
 class BoxOffice extends Actor {
-//  import BoxOffice._
-//  import context._
+  import BoxOffice._
+  import context._
 
-  override def receive: Receive = ???
+  override def receive: Receive = {
+    case GetEvents =>
+      import akka.pattern.pipe
+      import akka.pattern.ask
+
+      def getEvents = context.children.map { child =>
+        self.ask(GetEvent)
+      }
+      pipe()
+  }
 }

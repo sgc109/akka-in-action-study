f31229a8a077a017b487d2aca7f77571e39bf732
probe <re2rar@gmail.com>
2020-08-24 18:13:16 +0900

    TicketSeller Add, Buy test 추가.

--- probe/chapter-02/src/main/scala/com/probe/TicketSeller.scala
+++ probe/chapter-02/src/main/scala/com/probe/TicketSeller.scala
@@ -6,11 +6,30 @@ object TicketSeller {
   def props(event: String) = Props(new TicketSeller(event))
 
   case class Ticket(id: Int)
+
   case class Add(tickets: Vector[Ticket])
+
   case class Buy(tickets: Int)
+
   case class Tickets(event: String, entries: Vector[Ticket] = Vector.empty[Ticket])
+
 }
 
 class TicketSeller(event: String) extends Actor {
-  override def receive: Receive = ???
+
+  import TicketSeller._
+
+  var tickets = Vector.empty[Ticket]
+
+  override def receive: Receive = {
+    case Add(newTickets) => tickets = tickets ++ newTickets
+    case Buy(numberOfTickets) =>
+      val entries = tickets.take(numberOfTickets)
+      if (entries.size >= numberOfTickets) {
+        sender() ! Tickets(event, entries)
+        tickets = tickets.drop(numberOfTickets)
+      } else {
+        sender() ! Tickets(event)
+      }
+  }
 }
